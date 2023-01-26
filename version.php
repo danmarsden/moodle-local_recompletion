53601d1af976f52552715b82aaeac9c34d69dc69
Dan Marsden <dan@danmarsden.com>
2023-01-26 21:43:06 +1300

    bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2021100102;
-$plugin->release   = '2021100102';
+$plugin->version   = 2021100103;
+$plugin->release   = '2021100103';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2020061500; // Requires 3.9.
 $plugin->component = 'local_recompletion';
