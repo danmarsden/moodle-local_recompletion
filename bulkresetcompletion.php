1247cf20e56c2d7bcc344c8f0de9ab9e75518496
Yannick Forget <yannickforget@live.nl>
2023-03-23 15:59:21 +0100

    **new feature** in bulk resett all completion #63
    Added some new strings for the new feature
    
    Correction in strings
    
    requested changes made
    
    Applying pending changes

--- bulkresetcompletion.php
+++ bulkresetcompletion.php
@@ -15,99 +15,72 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Manually edit completion date for a course.
+ * Manually reset completion date for a course for a selected amount of users.
  *
- * @copyright 2020 Catalyst IT
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @package local_recompletion
+ * @package     local_recompletion
+ * @copyright   2023 Catalyst IT
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once('../../config.php');
-require_once($CFG->dirroot.'/user/lib.php');
-require_once($CFG->libdir.'/formslib.php');
-require_once($CFG->dirroot.'/local/recompletion/locallib.php');
-require_once($CFG->dirroot.'/course/lib.php');
-require_once($CFG->libdir.'/completionlib.php');
-require_once($CFG->libdir.'/gradelib.php');
-require_once($CFG->dirroot . '/mod/assign/locallib.php');
-require_once($CFG->dirroot . '/mod/quiz/lib.php');
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/../../config.php');
+require_once($CFG->dirroot . '/user/lib.php');
+require_once($CFG->libdir . '/formslib.php');
 
 $courseid = required_param('id', PARAM_INT);
 $userid   = optional_param('user', 0, PARAM_INT);
-$users   = optional_param_array('users', array(), PARAM_INT);
+$users    = optional_param_array('users', [], PARAM_INT);
 
-$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+$course = $DB->get_record('course', ['id' => $courseid], '*', MUST_EXIST);
 require_login($course);
 
 $context = context_course::instance($course->id);
 require_capability('local/recompletion:manage', $context);
 
-$PAGE->set_url('/local/recompletion/editcompletion.php', array('id' => $course->id));
+$PAGE->set_url('/local/recompletion/editcompletion.php', ['id' => $course->id]);
+
 if (empty($users) && empty($userid)) {
     // The first time list hack.
     if ($post = data_submitted()) {
         foreach ($post as $k => $v) {
             if (preg_match('/^user(\d+)$/', $k, $m)) {
-                $users[] = $m[1];
+                $users[] = (int) $m[1];
             }
         }
     }
+
     if (empty($users)) {
-        redirect($CFG->wwwroot.'/local/recompletion/participants.php?id='.$course->id,
+        redirect($CFG->wwwroot . '/local/recompletion/participants.php?id=' . $course->id,
             get_string('nousersselected', 'local_recompletion'));
     }
-
 }
+
 if (empty($users)) {
-    $users = array();
-    $users[] = $userid;
+    $users = [$userid];
     // Get this users current completion date and use that in the form.
-    $params = array(
-        'userid'    => $userid,
-        'course'    => $courseid
-    );
+    $params = ['userid' => $userid, 'course' => $courseid];
     $ccompletion = new \completion_completion($params);
     if ($ccompletion->is_complete()) {
         $date = $ccompletion->timecompleted;
     }
 }
+
 if (empty($date)) {
     // Use current time as default.
     $date = time();
 }
 
-// Function to reset completion for $users.
-$resetcompletion = optional_param('reset_completion', 0, PARAM_BOOL);
-if ($resetcompletion && confirm_sesskey()) {
-    $config = $DB->get_records_menu('local_recompletion_config', array('course' => $course->id), '', 'name, value');
-    $config = (object) $config;
-
-    foreach ($users as $user) {
-        $userid = $user;
-        $reset = new local_recompletion\task\check_recompletion();
-        $errors = $reset->reset_user($userid, $course, $config);
-    }
-
-    redirect($CFG->wwwroot.'/local/recompletion/participants.php?id='.$course->id,
-        get_string('completionreset', 'local_recompletion'));
-}
-
-
-
 $form = new local_recompletion_coursecompletion_form('editcompletion.php',
-    array('course' => $courseid, 'users' => $users, 'date' => $date));
+    ['course' => $courseid, 'users' => $users, 'date' => $date]);
 
 if ($form->is_cancelled()) {
-    redirect($CFG->wwwroot.'/local/recompletion/participants.php?id='.$course->id);
-
+    redirect($CFG->wwwroot . '/local/recompletion/participants.php?id=' . $course->id);
 } else if ($data = $form->get_data()) {
     if (!empty($data->newcompletion)) {
         // Update course completion.
         foreach ($users as $user) {
-            $params = array(
-                'userid'    => $user,
-                'course'    => $courseid
-            );
+            $params = ['userid' => $user, 'course' => $courseid];
             $ccompletion = new \completion_completion($params);
             if ($ccompletion->is_complete()) {
                 // If we already have a completion date, clear it first so that mark_complete works.
@@ -115,12 +88,13 @@ if ($form->is_cancelled()) {
             }
             $ccompletion->mark_complete($data->newcompletion);
         }
-        redirect($CFG->wwwroot.'/local/recompletion/participants.php?id='.$course->id,
+        redirect($CFG->wwwroot . '/local/recompletion/participants.php?id=' . $course->id,
             get_string('completionupdated', 'local_recompletion'));
     }
 }
 
 $userlist = user_get_users_by_id($users);
+
 echo $OUTPUT->header();
 echo $OUTPUT->heading(get_string('editcompletion', 'local_recompletion'));
 echo $OUTPUT->box(get_string('editcompletion_desc', 'local_recompletion'));
@@ -130,7 +104,9 @@ foreach ($userlist as $user) {
     echo html_writer::div(fullname($user));
 }
 echo html_writer::end_div();
+
 echo html_writer::start_div('userform');
 $form->display();
 echo html_writer::end_div();
+
 echo $OUTPUT->footer();
