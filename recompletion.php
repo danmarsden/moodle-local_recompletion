a4e620b9a4517531cb74c7cda4af4871bf5c0724
Kevin Pham <keevan.pham@gmail.com>
2023-09-28 13:17:21 +1000

    feat: add handling of scheduling fields for reset task
    - Reset based on period or schedule

--- recompletion.php
+++ recompletion.php
@@ -24,7 +24,6 @@
 
 require_once(__DIR__.'/../../config.php');
 require_once($CFG->dirroot.'/local/recompletion/locallib.php');
-require_once($CFG->dirroot.'/lib/adminlib.php');
 require_once($CFG->dirroot.'/course/lib.php');
 require_once($CFG->libdir.'/completionlib.php');
 require_once($CFG->libdir.'/formslib.php');
