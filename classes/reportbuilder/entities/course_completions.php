00d3e29f4aa68d423479ead298ca3b06fa33cdb7
jgonzcou <jgonzcou@gmail.com>
2025-04-26 01:28:39 +0200

    fix: update custom reports entities to use get_default_tables instead of deprecated get_default_table_aliases, in Moodle 4.5 (#209)

--- classes/reportbuilder/entities/course_completions.php
+++ classes/reportbuilder/entities/course_completions.php
@@ -34,13 +34,13 @@ use stdClass;
 class course_completions extends base {
 
     /**
-     * Database tables that this entity uses and their default aliases
+     * Database tables that this entity uses
      *
-     * @return string[] Array of $tablename => $alias
+     * @return string[]
      */
-    protected function get_default_table_aliases(): array {
+    protected function get_default_tables(): array {
         return [
-            'local_recompletion_cc' => 'cc'
+            'local_recompletion_cc',
         ];
     }
 
