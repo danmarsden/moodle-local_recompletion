6087b92378b682447ce6d7091656431999b37354
Dan Marsden <dan@danmarsden.com>
2026-06-05 19:35:11 +1200

    Bump version for release.

--- classes/local/restrictions/enrol.php
+++ classes/local/restrictions/enrol.php
@@ -55,7 +55,7 @@ class enrol extends base {
             $enrolplugins,
             $options
         );
-        if (!empty($config->restrictenrol)){
+        if (!empty($config->restrictenrol)) {
             $mform->setDefault('restrictenrol', $config->restrictenrol);
         }
 
--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2026022703;
-$plugin->release   = 2026022703;
+$plugin->version   = 2026022704;
+$plugin->release   = 2026022704;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2024100700; // Requires 4.5.
 $plugin->component = 'local_recompletion';
