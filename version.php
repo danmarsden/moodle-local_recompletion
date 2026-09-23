adb24f8046183148828e10e5338e91eb5ccef5a9
Dan Marsden <dan@danmarsden.com>
2026-09-23 22:57:29 +1200

    bump version for 5.3 release.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2026091800;
-$plugin->release   = 2026091800;
+$plugin->version   = 2026092300;
+$plugin->release   = 2026092300;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2026090300; // Requires 5.3.
 $plugin->component = 'local_recompletion';
